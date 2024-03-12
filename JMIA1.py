68b6aa6fbe5744bb4eabefb7934dffaf80b8c9db
Adbrap <adprog33@gmail.com>
2024-03-12 15:56:18 +0100

    first commit

--- JMIA1.py
+++ JMIA1.py
@@ -581,7 +581,7 @@ def Finder_IETE(time1, time_name1, start1):
                     if I[1] > B and J[
                         1] > F and moyenne_epaule1 <= moyenne_tete / 2 and moyenne_epaule2 <= moyenne_tete / 2 and moyenne_epaule1 >= moyenne_tete / 4 and moyenne_epaule2 >= moyenne_tete / 4 and accept == True and \
                             df['c'].values[-2] <= J[1] + (moyenne_tete) / 4 and df['c'].values[-2] >= J[1] and \
-                            df['c'].index[-2] != local_min[-1] and pourcent_chercher == True and G >= 1:
+                            df['c'].index[-2] != local_min[-1] and mini_pourcent == True and G >= 1:
                         # ----- condition pour filtrer iete  -----#
 
                         # ----- systeme de notation des iete en fonction de la beaute et de la perfection de realisation  -----#
